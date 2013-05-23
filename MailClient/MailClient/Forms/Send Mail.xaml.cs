522fd9055c6b84e0c1c16cfed5487c238783047e
Jacob Graungaard <kristensen8@hotmail.com>
2013-05-23 14:31:00 +0200

    back

--- MailClient/MailClient/Forms/Send Mail.xaml.cs
+++ MailClient/MailClient/Forms/Send Mail.xaml.cs
@@ -24,5 +24,10 @@ namespace MailClient.Forms
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new MyMainMenu());
+        }
     }
 }
